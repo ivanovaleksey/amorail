06c8c576af3bc09b2215d0891af58923262dcf4b
Aleksey Ivanov <ialexxei@gmail.com>
2017-06-19 13:30:04 +0300

    Fix "undefined method `AmoUnknownError'"

--- lib/amorail/client.rb
+++ lib/amorail/client.rb
@@ -95,7 +95,7 @@ module Amorail
       when 503
         fail ::Amorail::AmoServiceUnaviableError
       else
-        fail ::Amorail::AmoUnknownError(response.body)
+        fail ::Amorail::AmoUnknownError, response.body
       end
     end
 
